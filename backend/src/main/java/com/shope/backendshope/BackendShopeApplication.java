786fc62b2b96288e1406d7162de1a221ea007032
Tom Baxter <tbaxter@atlassian.com>
2021-04-12 20:35:35 +1000

    Moved backend into the repo

---
+++ backend/src/main/java/com/shope/backendshope/BackendShopeApplication.java
@@ -0,0 +1,13 @@
+package com.shope.backendshope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendShopeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shope/backendshope/BackendShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shope.backendshope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
