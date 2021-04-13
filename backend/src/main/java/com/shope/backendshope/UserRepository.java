df1f0f5ace6362df0393d5be5c5de0fc37a4df06
Tom Baxter <tbaxter@atlassian.com>
2021-04-13 21:23:03 +1000

    Swapped out to boilerplate, no changes yet

--- backend/src/main/java/com/shope/backendshope/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shope.backendshope;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface UserRepository extends Neo4jRepository<User, Long> {
-
-    User getUserByUsername(String username);
-}
